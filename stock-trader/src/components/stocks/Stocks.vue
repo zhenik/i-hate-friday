6750a0abaac644acd08adb09e33869d500e3c7a8
zhenik <nikita.zhevnitskiy@gmail.com>
2020-01-31 22:13:48 +0100

    add stocks tmpl

--- stock-trader/src/components/stocks/Stocks.vue
+++ stock-trader/src/components/stocks/Stocks.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>The Stocks Component</h1>
+  <div>
+    <app-stock v-for="stock in stocks" v-bind:key="stock.id"></app-stock>
+  </div>
 </template>
+
+<script>
+  import Stock from './Stock.vue'
+  export default {
+    data() {
+      return {
+        stocks: [
+          {id:1, name: 'BMW', price: 110},
+          {id:2, name: 'GOOGLE', price: 200},
+          {id:3, name: 'APPLE', price: 250},
+          {id:4, name: 'TAPAD', price: 8},
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
