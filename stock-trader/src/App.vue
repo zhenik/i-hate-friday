4350a516d37e3fc1ebb710f37053b8e2bc1c639f
zhenik <nikita.zhevnitskiy@gmail.com>
2020-01-31 22:02:22 +0100

    add botstrap

--- stock-trader/src/App.vue
+++ stock-trader/src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="container">
+    <app-header>
+      <div class="row">
+        <div class="col-xs-12">
+          <router-view></router-view>
+        </div>
+      </div>
+    </app-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-
+import Header from './components/Header.vue';
 export default {
-
+  components: {
+    appHeader: Header
+  }
 }
 </script>
 
 <style>
-
+  body {
+    padding: 30px;
+  }
 </style>
