df303285261c41cbf4f33aaf243ebececa5e472f
zhenik <nikita.zhevnitskiy@gmail.com>
2020-01-31 21:47:26 +0100

    add routing

--- stock-trader/src/main.js
+++ stock-trader/src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+import App from './App.vue'
+import { routes } from './routes.js'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+Vue.config.productionTip = false;
 
 new Vue({
+  el: '#app',
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
