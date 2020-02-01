84849bf2c109da629ac677f0b2ba5e6ccd193282
zhenik <nikita.zhevnitskiy@gmail.com>
2020-02-01 22:23:54 +0100

    animation

--- stock-trader/src/App.vue
+++ stock-trader/src/App.vue
@@ -1,14 +1,12 @@
 <template>
-  <div id="app">
-    <div class="container">
-      <app-header>
-        <div class="row">
-          <div class="col-xs-12">
-            <router-view></router-view>
-          </div>
-        </div>
-      </app-header>
-      <router-view></router-view>
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-xs-12">
+        <transition name="slide" mode="out-in">
+          <router-view></router-view>
+        </transition>
+      </div>
     </div>
   </div>
 </template>
@@ -28,5 +26,35 @@
 <style>
   body {
     padding: 30px;
+  }
+
+  .slide-enter-active {
+    animation: slide-in 200ms ease-out forwards;
+  }
+
+  .slide-leave-active {
+    animation: slide-out 200ms ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(-30px);
+      opacity: 0;
+    }
+    to {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+      opacity: 1;
+    }
+    to {
+      transform: translateY(-30px);
+      opacity: 0;
+    }
   }
 </style>
